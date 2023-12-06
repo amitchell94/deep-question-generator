35aac7ea0b00cee2ac2147329956f61713a79fa9
MITCHELL Andrew <andrew.mitchell@vinci-energies.net>
2023-12-06 18:17:31 +0000

    first attempt

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+/*!
+* Start Bootstrap - Heroic Features v5.0.2 (https://startbootstrap.com/template/heroic-features)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-heroic-features/blob/master/LICENSE)
+*/
+function showRandomQuestion() {
+    fetch('../questions.json') // Path to your JSON file
+        .then(response => response.json())
+        .then(questions => {
+            const randomQuestion = questions[Math.floor(Math.random() * questions.length)];
+            document.getElementById('question').innerText = randomQuestion;
+        })
+        .catch(error => console.error('Error loading questions:', error));
+}
