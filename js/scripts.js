9383e7bd4f78e2e40d260c2f5964a123386233f3
MITCHELL Andrew <andrew.mitchell@vinci-energies.net>
2023-12-06 18:48:54 +0000

    added list of all questions + cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,18 @@ function showRandomQuestion() {
         })
         .catch(error => console.error('Error loading questions:', error));
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('questions.json') // Adjust the path as necessary
+        .then(response => response.json())
+        .then(questions => {
+            const listElement = document.getElementById('questions-list');
+            questions.forEach(question => {
+                const listItem = document.createElement('li');
+                listItem.className = "list-group-item"
+                listItem.textContent = question;
+                listElement.appendChild(listItem);
+            });
+        })
+        .catch(error => console.error('Error loading questions:', error));
+});
